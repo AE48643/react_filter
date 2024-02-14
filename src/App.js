5848ff7cc2e5298124ffb9c72195b6a928c8756b
AE48643 <122492547+AE48643@users.noreply.github.com>
2024-02-14 21:12:24 +0000

    Filter Done!

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  // Declare state variables
+  const [filter, setFilter] = useState('');
+  const [data, setData] = useState([
+    ['Eli', 'Germany'],
+    ['Smith', 'Sweden'],
+    ['Job', 'UK'],
+    ['Jacob', 'Germany'],
+  ]);
 
-    // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  console.log(input)
-  
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-        txtValue = td.textContent || td.innerText;
+  const handleInputChange = (event) => {
+    setFilter(event.target.value.toUpperCase());
+  };
+
+  useEffect(() => {
+    // Use effect to handle filtering when the filter state changes
+    const table = document.getElementById("myTable");
+    const tr = table.getElementsByTagName("tr");
+
+    for (let i = 0; i < tr.length; i++) {
+      const td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        const txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          // Need to change the java/css elements to reactJS
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
         }
       }
     }
+  }, [filter]);
 
   return (
-    <div className="App">
-        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for names.."></input>
-        <h1>{input}</h1>
+    <div>
+      <input
+        type="text"
+        value={filter}
+        onChange={handleInputChange}
+        placeholder="Search for names.."
+      />
 
-        <table id="myTable">
-          <tr class="header">
-            <th>Name</th>
-            <th>Country</th>
-          </tr>
+      <table id="myTable">
+        <thead>
           <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Germany</td>
+            <th style={{ width: '60%' }}>Name</th>
+            <th style={{ width: '40%' }}>Country</th>
           </tr>
-          <tr>
-            <td>Berglunds snabbkop</td>
-            <td>Sweden</td>
-          </tr>
-          <tr>
-            <td>Island Trading</td>
-            <td>UK</td>
-          </tr>
-          <tr>
-            <td>Koniglich Essen</td>
-            <td>Germany</td>
-          </tr>
-        </table>
-    </div>);
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>{row[0]}</td>
+              <td>{row[1]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
